75836929802eda0a08fe95114162d5788780c956
Pedro Paulo <pvianna@ersoft.com.br>
2024-07-28 23:24:32 -0300

    adjust to user information page

--- src/app/feature/user-information/user-information.component.ts
+++ src/app/feature/user-information/user-information.component.ts
@@ -9,7 +9,11 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../core/services/user.service';
+import { catchError, map, Observable, of } from 'rxjs';
+import { SignInService } from '../../core/services/sign-in.service';
+import { LoadingSpinnerComponent } from '../../shared/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-user-information',
@@ -21,29 +25,70 @@ import { RouterModule } from '@angular/router';
     MatCardModule,
     MatInputModule,
     MatButtonModule,
+    LoadingSpinnerComponent,
   ],
   templateUrl: './user-information.component.html',
   styleUrl: './user-information.component.scss',
 })
 export class UserInformationComponent {
   private formBuilder: FormBuilder = inject(FormBuilder);
+  private userService: UserService = inject(UserService);
+  private tokenService: SignInService = inject(SignInService);
+  private router: Router = inject(Router);
+
+  public userInfo$?: Observable<FormGroup>;
 
   @Input('error-message') error: string | null = null;
   @Output('login') submitEM = new EventEmitter();
 
-  public form: FormGroup = this.formBuilder.group({
-    username: ['', Validators.required],
-    nickname: ['', Validators.required],
-    email: ['', Validators.required],
-    newPassword: ['', Validators.required],
-    currentPassword: ['', Validators.required],
-  });
+  ngOnInit(): void {
+    let tokenEmail = this.tokenService.getTokenEmail();
+    console.log('Email from token', tokenEmail);
 
-  ngOnInit(): void {}
+    this.userInfo$ = this.userService.getUserInfo(tokenEmail).pipe(
+      map((user) => {
+        console.log('Email from token observer', tokenEmail);
+        console.log('User', user);
+        let form = this.formBuilder.group({
+          username: [user?.nome ?? '', Validators.required],
+          nickname: [user?.nickName ?? '', Validators.required],
+          email: [user?.email ?? '', Validators.required],
+          phone: [user?.phoneNumber ?? '', Validators.nullValidator],
+          currentPassword: ['', Validators.required],
+          newPassword: ['', Validators.required],
+        });
 
-  submit() {
-    if (this.form.valid) {
-      this.submitEM.emit(this.form.value);
+        return form;
+      }),
+      catchError((err) => {
+        console.error(err);
+        return of(
+          this.formBuilder.group({
+            username: ['', Validators.required],
+            nickname: ['', Validators.required],
+            email: ['', Validators.required],
+            phone: ['', Validators.nullValidator],
+            currentPassword: ['', Validators.required],
+            newPassword: ['', Validators.required],
+          })
+        );
+      })
+    );
+  }
+
+  submit(form: FormGroup) {
+    console.log(form.getRawValue());
+
+    if (form.valid) {
+      this.submitEM.emit(form.value);
     }
+  }
+
+  logout() {
+    if (this.tokenService.isAuthenticated()) {
+      this.tokenService.logout();
+    }
+
+    this.router.navigate(['/']);
   }
 }
