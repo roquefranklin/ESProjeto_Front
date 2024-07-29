f8884e03037d3ba9bcf4b852c105a9589fc79327
Pedro Paulo <pvianna@ersoft.com.br>
2024-07-28 23:22:09 -0300

    chore: navbar button update

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SignInService } from '../services/sign-in.service';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  public tokenService: SignInService = inject(SignInService);
 
 }
