4b4a1ec5023454134d8c97b2402ece095d0e7e69
Pedro Paulo <pvianna@ersoft.com.br>
2024-07-28 23:21:32 -0300

    chore: methods to token management

--- src/app/core/services/sign-in.service.ts
+++ src/app/core/services/sign-in.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { jwtDecode, JwtHeader } from 'jwt-decode';
 import { environment } from '../../../environments/environment';
+import { Token } from '../../shared/models/token';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +13,61 @@ export class SignInService {
 
   constructor() {}
 
-  public SignIn(email: string, password: string): Observable<string> {
-    return this.http.post<string>(`${environment.BASE_URL}/token/signin`, {
-      email,
-      password,
-    });
+  public SignIn(email: string, password: string): Observable<Token> {
+    return this.http
+      .post<Token>(`${environment.BASE_URL}/Token/signin`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          localStorage.setItem(
+            'access_token',
+            JSON.stringify(response.accessToken.toString())
+          );
+          localStorage.setItem(
+            'refresh_token',
+            JSON.stringify(response.refreshToken.toString())
+          );
+        })
+      );
+  }
+
+  public getRefresgToken() {
+    return JSON.parse(localStorage.getItem('refresh_token') ?? '{}');
+  }
+
+  public getAccessToken() {
+    return JSON.parse(localStorage.getItem('access_token') ?? '{}');
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token');
+    return !!token;
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+  }
+
+  getTokenEmail() {
+    let token = this.getAccessToken();
+
+    let email = this.processEmailResult(
+      (jwtDecode<JwtHeader>(token) as unknown as any).email
+    );
+
+    return email;
+  }
+
+  private processEmailResult(result: string | string[]): string {
+    if (typeof result === 'string') {
+      return result;
+    } else if (Array.isArray(result) && result.length > 0) {
+      return result[0];
+    } else {
+      return '';
+    }
   }
 }
