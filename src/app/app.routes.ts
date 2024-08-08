e28991edd85d45df43264128cc05f5754ffd73f9
Pedro Paulo <pvianna@ersoft.com.br>
2024-08-08 20:50:40 -0300

    chore: reorganization of routes and login guard for sign in and sign up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,35 @@ import { SignInComponent } from './feature/sign-in/sign-in.component';
 import { HomeComponent } from './feature/home/home.component';
 import { SignUpComponent } from './feature/sign-up/sign-up.component';
 import { UserInformationComponent } from './feature/user-information/user-information.component';
+import { AuthenticationGuard } from './core/guards/authenticate.guard';
+import { TemplateComponent } from './core/template/template.component';
+import { RedirectLoggedToHomeGuard } from './core/guards/redirec-logged-to-home.guard';
 
 export const routes: Routes = [
   {
     path: 'sign-in',
+    canActivate: [RedirectLoggedToHomeGuard],
     component: SignInComponent,
   },
   {
     path: 'sign-up',
+    canActivate: [RedirectLoggedToHomeGuard],
     component: SignUpComponent,
   },
   {
-    path: 'user-information',
-    component: UserInformationComponent,
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
+    path: '',
+    component: TemplateComponent,
+    canActivate: [AuthenticationGuard],
+    children: [
+      {
+        path: 'user-information',
+        component: UserInformationComponent,
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ],
   },
   {
     path: '**',
