98efe690de44ab7b8b762d04992c82734e8ed24b
Pedro Paulo <pvianna@ersoft.com.br>
2024-08-08 20:30:17 -0300

    chore: removal of headers from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
-import { TemplateComponent } from './core/template/template.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavBarComponent, TemplateComponent],
+  imports: [RouterOutlet, HeaderComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
