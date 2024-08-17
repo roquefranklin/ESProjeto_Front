14a8cadea99182757edd3aa1461c99924a9495b5
Pedro Paulo <pvianna@ersoft.com.br>
2024-08-17 16:19:35 -0300

    chore: ajuste em botão de logout de header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SignInService } from '../services/sign-in.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  public tokenService: SignInService = inject(SignInService);
+  private router: Router = inject(Router);
 
+  logout() {
+    if (this.tokenService.isAuthenticated()) {
+      this.tokenService.logout();
+    }
+
+    this.router.navigate(['/sign-in']);
+  }
 }
