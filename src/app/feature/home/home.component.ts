1488cae1dcab5c23f3eddf5a03f5f194aa6c161d
homologacaoweb <webmaster@demaria.com.br>
2024-08-28 21:50:34 -0300

    feat: primeiros passos do mapa

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -2,7 +2,14 @@ import { Component, inject, OnDestroy, OnInit, TemplateRef, ViewChild } from '@a
 import { BottomMenuManagerService } from '../../core/services/bottom-menu-manager.service';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { RouterLink } from '@angular/router';
-import * as Leaflet from 'leaflet';
+import * as L from 'leaflet';
+
+const layOsm: L.TileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  minZoom: 19,
+  attribution: 'Map-Name',
+  detectRetina: true
+});
 
 @Component({
   selector: 'app-home',
@@ -11,53 +18,100 @@ import * as Leaflet from 'leaflet';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-
-export class HomeComponent implements OnInit, OnDestroy{
-  
+export class HomeComponent implements OnInit, OnDestroy {
   latitude: number = 0;
   longitude: number = 0;
   watchId: number = 0;
+  map: L.Map | undefined;
+  marker: L.Marker | undefined;
+  circle: L.Circle | undefined;
 
-  map: Leaflet.Map | undefined;
-  
-  constructor() {
-  }
-  private menuButtonService = inject(BottomMenuManagerService);
-  
-  
-  @ViewChild('homeMenuButtons') menuOptions!: TemplateRef<any>;
-  
-  options: Leaflet.MapOptions = {
-    layers: getLayers(),
-    zoom: 10,
-    center: new Leaflet.LatLng(this.latitude, this.longitude)
+  leafletOptions: L.MapOptions = {
+    zoom: 15,
+    zoomControl: false,
+    dragging: false,        // Desabilita arrastar o mapa
+    scrollWheelZoom: false, // Desabilita zoom com a roda do mouse
+    doubleClickZoom: false, // Desabilita zoom com duplo clique
+    boxZoom: false,         // Desabilita zoom com box (clicar e arrastar)
+    keyboard: false,        // Desabilita controle via teclado
+    touchZoom: false,       // Desabilita zoom por toque (em dispositivos móveis)
+    inertia: false          // Desabilita o efeito de inércia ao arrastar o mapa
   };
+
+  baseLayers: { [layerName: string]: L.Layer } = {
+    'openstreetmap': layOsm
+  };
+  layersControlOptions: L.ControlOptions = { position: 'topright' };
+
+  constructor() { }
+
+  private menuButtonService = inject(BottomMenuManagerService);
+
+  @ViewChild('homeMenuButtons') menuOptions!: TemplateRef<any>;
+
   ngOnInit(): void {
     this.watchUserLocation();
   }
+
   ngAfterViewInit(): void {
     this.menuButtonService.setMenuOption(this.menuOptions);
   }
-  
+
   ngOnDestroy(): void {
-    this.clearWatch()
+    this.clearWatch();
   }
-  
+
+  onMapReady(map: L.Map) {
+    this.map = map;
+    console.log(map);
+  }
+
   watchUserLocation(): void {
     if (navigator.geolocation) {
       this.watchId = navigator.geolocation.watchPosition(
         (position) => {
-          this.latitude = position.coords.latitude
-          this.longitude = position.coords.longitude
-          this.map?.panTo(new Leaflet.LatLng(this.latitude, this.longitude))
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+          const latLng = new L.LatLng(this.latitude, this.longitude);
+
+          // Centraliza o mapa na nova coordenada
+          this.map?.panTo(latLng);
+
+          // Remove o marcador e círculo anterior, se existirem
+          if (this.marker) {
+            this.map?.removeLayer(this.marker);
+          }
+          if (this.circle) {
+            this.map?.removeLayer(this.circle);
+          }
+
+          // Adiciona o marcador personalizado
+          this.marker = new L.Marker(latLng, {
+            icon: new L.Icon({
+              iconSize: [50, 41],
+              iconAnchor: [13, 41],
+              iconUrl: 'assets/blue-marker.svg',
+            }),
+            title: 'Você está aqui'
+          }).addTo(this.map!);
+
+          // Desenha um círculo azul ao redor do marcador
+          this.circle = L.circle(latLng, {
+            radius: 30, // 30m de raio
+            color: 'blue',
+            fillColor: '#30f',
+            fillOpacity: 0.2
+          }).addTo(this.map!);
+
+          console.log(latLng);
         },
         (error) => {
           console.error('Erro ao obter localização: ', error);
         },
         {
-          enableHighAccuracy: true, // Tenta obter a localização mais precisa possível
-          timeout: 5000,            // Tempo máximo para obter a localização (em milissegundos)
-          maximumAge: 0             // Não usa cache; sempre tenta obter uma nova posição
+          enableHighAccuracy: true,
+          timeout: 50,
+          maximumAge: 5
         }
       );
     } else {
@@ -71,69 +125,3 @@ export class HomeComponent implements OnInit, OnDestroy{
     }
   }
 }
-
-export const getMarkers = (): Leaflet.Marker[] => {
-  return [
-    new Leaflet.Marker(new Leaflet.LatLng(43.5121264, 16.4700729), {
-      icon: new Leaflet.Icon({
-        iconSize: [50, 41],
-        iconAnchor: [13, 41],
-        iconUrl: 'assets/blue-marker.svg',
-      }),
-      title: 'Workspace'
-    } as Leaflet.MarkerOptions),
-    new Leaflet.Marker(new Leaflet.LatLng(43.5074826, 16.4390046), {
-      icon: new Leaflet.Icon({
-        iconSize: [50, 41],
-        iconAnchor: [13, 41],
-        iconUrl: 'assets/red-marker.svg',
-      }),
-      title: 'Riva'
-    } as Leaflet.MarkerOptions),
-  ] as Leaflet.Marker[];
-};
-
-export const getRoutes = (): Leaflet.Polyline[] => {
-  return [
-    new Leaflet.Polyline([
-      new Leaflet.LatLng(43.5121264, 16.4700729),
-      new Leaflet.LatLng(43.5074826, 16.4390046),
-    ] as Leaflet.LatLng[], {
-      color: '#0d9148'
-    } as Leaflet.PolylineOptions)
-  ] as Leaflet.Polyline[];
-};
-
-export const getLayers = (): Leaflet.Layer[] => {
-  return [
-    // Basic style
-    new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors',
-    } as Leaflet.TileLayerOptions),
-    // Pastel style, remove if you want basic style. Uncomment if you want pastel style.
-    new Leaflet.TileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key={your_key}', {
-      attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
-    } as Leaflet.TileLayerOptions),
-    // new Leaflet.TileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key={your_key}', {
-    //   attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
-    // } as Leaflet.TileLayerOptions),
-    ...getMarkers(),
-    ...getRoutes(),
-    ...getPolygons()
-  ] as Leaflet.Layer[];
-};
-
-export const getPolygons = (): Leaflet.Polygon[] => {
-  return [
-    new Leaflet.Polygon([
-      new Leaflet.LatLng(43.5181349, 16.4537963),
-      new Leaflet.LatLng(43.517890, 16.439939),
-      new Leaflet.LatLng(43.515599, 16.446556),
-      new Leaflet.LatLng(43.518025, 16.463492)
-    ] as Leaflet.LatLng[],
-      {
-        fillColor: '#eb530d',
-        color: '#eb780d'
-      } as Leaflet.PolylineOptions)
-  ] as Leaflet.Polygon[];
-};
